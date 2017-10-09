add439beaeed203b09c7ccb41809df98282d1e8e
jp112sdl <jp112sdl@icloud.com>
2017-10-09 15:47:11 +0200

    added new firmware version #18

--- SonoffHMLOX.ino
+++ SonoffHMLOX.ino
@@ -23,9 +23,9 @@
 #include "js_pow.h"
 #include "js_fwupd.h"
 
-const String FIRMWARE_VERSION = "1.0.9";
+const String FIRMWARE_VERSION = "1.0.10";
 //#define                       UDPDEBUG
-#define                       SERIALDEBUG
+//#define                       SERIALDEBUG
 
 #define LEDPinSwitch          13
 #define LEDPinPow             15
