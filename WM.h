2b1a88f19fd5483ee02bf59e91bc3929270a2f11
jp112sdl <jp112sdl@icloud.com>
2018-03-20 20:11:11 +0100

    adding wifimanager feature to clear all data (like factory reset)

--- WM.h
+++ WM.h
@@ -26,7 +26,7 @@ extern "C" {
 
 const char HTTP_HEAD[] PROGMEM            = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><title>{v}</title>";
 const char HTTP_HEAD_END[] PROGMEM        = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
-const char HTTP_PORTAL_OPTIONS[] PROGMEM  = "<form action=\"/wifi\" method=\"get\"><button>Sonoff konfigurieren</button></form><br/><br/><form action=\"/i\" method=\"get\"><button>Chip-Info</button></form><br/><form action=\"/r\" method=\"post\"><button>Reset</button></form>";
+const char HTTP_PORTAL_OPTIONS[] PROGMEM  = "<form action=\"/wifi\" method=\"get\"><button>Sonoff konfigurieren</button></form><br/><br/><form action=\"/i\" method=\"get\"><button>Chip-Info</button></form><br/><form action=\"/r\" method=\"post\"><button>Restart</button></form>";
 const char HTTP_ITEM[] PROGMEM            = "<div><a href='#p' onclick='c(this)'>{v}</a>&nbsp;<span class='q {i}'>{r}%</span></div>";
 const char HTTP_FORM_START[] PROGMEM      = "<form method='get' action='wifisave'><div><input type='text' id='s' name='s' length=32 placeholder='SSID'></div><div><input id='p' name='p' length=64 type='password' placeholder='WLAN-Key'></div>";
 const char HTTP_FORM_PARAM_INPUT[] PROGMEM= "<div id='div_{i}'><input type='text' id='{i}' name='{n}' length={l} placeholder='{p}' value='{v}' {c}></div>";
@@ -36,6 +36,7 @@ const char HTTP_FORM_PARAM_PWD[] PROGMEM   = "<div id='div_{i}'><input type='pas
 
 const char HTTP_FORM_END[] PROGMEM        = "<div><button type='submit'>Speichern</button></div></form>";
 const char HTTP_SCAN_LINK[] PROGMEM       = "<br/><div class=\"c\"><a href=\"/wifi\">Scan</a></div>";
+const char HTTP_CLEARALLDATA_LINK[] PROGMEM = "<br/><div class=\"c\"><a href=\"/c\">Clear All Data</a></div>";
 const char HTTP_SAVED[] PROGMEM           = "<div>Daten gespeichert!<br/>Die WLAN-Netzwerkverbindung wird hergestellt...<br/><br/>Sollte keine Verbindung zustandekommen, bitte nochmals mit dem AP verbinden!</div>";
 const char HTTP_END[] PROGMEM             = "</div><script>setBackendType();</script></body></html>";
 
@@ -164,6 +165,7 @@ class WiFiManager
     void          handleWifiSave();
     void          handleInfo();
     void          handleReset();
+    void          handleClearAllData();
     void          handleNotFound();
     void          handle204();
     boolean       captivePortal();
