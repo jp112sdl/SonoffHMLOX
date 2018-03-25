fccdd772a64dc125e2f0c30ba7a3c4367b0d6403
jp112sdl <jp112sdl@icloud.com>
2018-03-25 21:00:08 +0200

    added static ip fix

--- WifiManager.ino
+++ WifiManager.ino
@@ -15,14 +15,14 @@ bool doWifiConnect() {
 
   if (!startWifiManager && _ssid != "" && _psk != "" ) {
     DEBUG(F("Connecting WLAN the classic way..."));
-    //WiFi.disconnect();
+    WiFi.disconnect();
     WiFi.mode(WIFI_STA);
     WiFi.hostname(GlobalConfig.Hostname);
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  
     WiFi.begin(_ssid.c_str(), _psk.c_str());
     int waitCounter = 0;
-    if (String(SonoffNetConfig.ip) != "0.0.0.0")
-      WiFi.config(IPAddress(ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3]), IPAddress(gwBytes[0], gwBytes[1], gwBytes[2], gwBytes[3]), IPAddress(netmaskBytes[0], netmaskBytes[1], netmaskBytes[2], netmaskBytes[3]));
+    if (String(SonoffNetConfig.ip) != "0.0.0.0") WiFi.config(IPAddress(ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3]), IPAddress(gwBytes[0], gwBytes[1], gwBytes[2], gwBytes[3]), IPAddress(netmaskBytes[0], netmaskBytes[1], netmaskBytes[2], netmaskBytes[3]));
 
     while (WiFi.status() != WL_CONNECTED) {
       waitCounter++;
@@ -34,6 +34,9 @@ bool doWifiConnect() {
       }
       delay(500);
     }
+    //sometimes static ip setting is not working - so we set it again
+    if (String(SonoffNetConfig.ip) != "0.0.0.0") WiFi.config(IPAddress(ipBytes[0], ipBytes[1], ipBytes[2], ipBytes[3]), IPAddress(gwBytes[0], gwBytes[1], gwBytes[2], gwBytes[3]), IPAddress(netmaskBytes[0], netmaskBytes[1], netmaskBytes[2], netmaskBytes[3]));
+
     DEBUG("Wifi Connected");
     WiFiConnected = true;
     return true;
